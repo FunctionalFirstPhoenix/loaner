5ff82f981840e9f440307f2d3ae8b046306f6dd0
David Koontz <david@koontzfamily.org>
2017-09-05 21:20:27 -0700

    September meeting progress, got JSON decoding working

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,22 @@
 module Main exposing (..)
 
-import Html exposing (beginnerProgram, div, button, text, input, br, p, Html)
+import Html exposing (program, div, button, text, input, br, p, Html)
 import Html.Events exposing (onClick, onCheck)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (style, id, type_, name, checked)
 import Date exposing (Date, fromTime)
 import Date.Extra.Config.Config_en_us as EnUs
 import Date.Extra.Format as Format
 import Dict exposing (Dict, empty, insert)
 import Domain exposing (..)
 import Debug
+import Http exposing (Error)
+import Json.Decode exposing (list, int, string, float, nullable, Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 
 
 model : Model
 model =
-    { accounts =
-        empty
-            |> insert 0
-                { id = 0
-                , owner = { firstName = "Bob", lastName = "Smith" }
-                , balance = 100
-                , accountType = Platinum
-                , status = Suspended (fromTime 1175820019000) Voluntary
-                }
-            |> insert 1
-                { id = 1
-                , owner = { firstName = "Janet", lastName = "Smyth" }
-                , balance = 3000
-                , accountType = Platinum
-                , status = Active (fromTime 1175820019000)
-                }
+    { accounts = empty
     }
 
 
@@ -66,8 +54,47 @@ statusToString status =
                         "voluntary"
 
 
+accountDecoder =
+    decode Account
+        |> required "id" int
+        |> required "owner" personDecoder
+        |> required "balance" int
+        --|> required "accountType" accountTypeDecoder
+        |> hardcoded Gold
+        --|> required "accountStatus" accountStatusDecoder
+        |> hardcoded (Active (fromTime 1175820019000))
+
+
+personDecoder : Decoder Person
+personDecoder =
+    decode Person
+        |> required "firstName" string
+        |> required "lastName" string
+
+
+
+{-
+   accountTypeDecoder : Decoder AccountType
+   accountTypeDecoder =
+       hardcoded Gold
+
+
+   accountStatusDecoder =
+       hardcoded (Active (fromTime 1175820019000))
+-}
+
+
+loadAccountData =
+    Http.send AccountDataRecieved (Http.get "http://localhost:8000/src/accounts.json" (list accountDecoder))
+
+
 main =
-    beginnerProgram { model = model, view = view, update = update }
+    program
+        { init = ( model, loadAccountData )
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
 
 
 accountTypeButtons currentAccountId currentAccountType =
@@ -110,10 +137,11 @@ view model =
 
 type Msg
     = ChangeAccountType AccountId AccountType
+    | AccountDataRecieved (Result Error (List Account))
     | NoOp
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeAccountType accountId theUpdatedAccountType ->
@@ -126,10 +154,26 @@ update msg model =
                         updatedAccounts =
                             Dict.insert accountId { account | accountType = theUpdatedAccountType } model.accounts
                     in
-                        { model | accounts = updatedAccounts }
+                        ( { model | accounts = updatedAccounts }, Cmd.none )
+
+        AccountDataRecieved result ->
+            case result of
+                Err error ->
+                    Debug.crash (toString error)
+
+                Ok accounts ->
+                    let
+                        foo =
+                            Debug.log "" accounts
+                        accountList =
+                        -- this is broken because accounts is a List Account not
+                        -- a Dict Int Account, convert it first then assign
+                    in
+
+                        ( { model | accounts = accountList }, Cmd.none )
 
         NoOp ->
-            model
+            ( model, Cmd.none )
 
 
 accountBackgroundColor : AccountType -> String
